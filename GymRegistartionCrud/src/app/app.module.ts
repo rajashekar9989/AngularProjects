86a1f6ddf2022054b3d0842731ca62f1c84891e1
rajashekar <rajas9162@.com>
2023-08-25 11:31:58 +0530

    Angular Crud Project

---
+++ GymRegistartionCrud/src/app/app.module.ts
@@ -0,0 +1,77 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NgToastModule, } from 'ng-angular-popup';
+import { NgConfirmModule } from 'ng-confirm-box';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+
+@NgModule({
+
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+
+  declarations: [
+    AppComponent,
+    RegistrationListComponent,
+    CreateRegistrationComponent,
+    UserDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule,
+    MatListModule,
+    MatChipsModule,
+
+    NgToastModule,
+    NgConfirmModule,
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
